ef9a724fa12633720dc7fcef221639d4ecd08507
IsmaelF01 <ismaelfloressanchez@gmail.com>
2020-11-01 19:01:26 +0100

    Subida de la practica2 ahorcado

--- Tema 3/Practica2/Ahorcado/nuevo_juego.php
+++ Tema 3/Practica2/Ahorcado/nuevo_juego.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
-include_once("_cabecera.php");
-
-
-
+include_once("cabecera.php");
+//Para cuando le des clic al boton New Game borra la sesion y empieza todo de nuevo
 if (isset($_GET['juego'])) {
     if ($_GET['juego'] == 'nuevo') {
         session_destroy();
     }
 }
-
-
 header("location: index.php");
